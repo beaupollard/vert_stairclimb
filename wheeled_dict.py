aa40da35b63bbedfaa3ea1c5ded5ab7a23bd49be
Beau Pollard <beauppollard@gmail.com>
2022-06-15 14:14:35 -0400

    Added a .py file that contains the parameter dictionary

---
+++ wheeled_dict.py
@@ -0,0 +1,73 @@
+sim_dict = {
+    "wheelbase_mean" : 0.4,#0.46,
+    "wheelbase_std" :  0.0,
+    "wheelbase_llim" : 0.3,
+    "wheelbase_ulim" :  0.6,
+
+    "payload_xloc_mean" : 0.1905,#0.25,#0.25,
+    "payload_xloc_std" :  0.0,#0.2,
+    "payload_xloc_llim" :  0.0,#-0.2,
+    "payload_xloc_ulim" :  0.4,
+
+    "payload_zloc_mean" : -0.1016,#-0.16,#-0.1,
+    "payload_zloc_std" :  0.0,#0.1, 
+    "payload_zloc_llim" :  -0.2,
+    "payload_zloc_ulim" :  0.07,#0.1,  
+
+    "wheel_size_mean" : 0.158,#0.1778,
+    "wheel_size_std" : 0.0,
+    "wheel_size_llim" : 0.15,
+    "wheel_size_ulim" : 0.305,
+
+    "sub_wheel_size_mean" : 0.05715,
+    "sub_wheel_size_std" : 0.00,
+    "sub_wheel_size_llim" : 0.01,
+    "sub_wheel_size_ulim" : 0.15,
+
+    "wheel_num_mean" : 3,
+    "wheel_num_std" : 0,
+    "wheel_num_llim" : 3,
+    "wheel_num_ulim" : 6, 
+
+    "step_num_mean" : 11,
+    "step_num_std" : 0,
+    "step_num_llim" : 1,
+    "step_num_ulim" : 1000,
+
+    "step_rise_mean" : 0.1778,
+    "step_rise_std" : 0.0,#0.0254,
+    "step_rise_llim" : 0.1524,
+    "step_rise_ulim" : 0.2413,
+
+    "step_slope_mean" : 35,
+    "step_slope_std" : 0,
+    "step_slope_llim" : 30,
+    "step_slope_ulim" : 45,
+
+    "payload_mean" : 60,
+    "payload_std" : 0,
+    "payload_llim" : 0,
+    "payload_ulim" : 455,    
+
+    "friction_mean" : 0.6,
+    "friction_std" : 0.3,
+    "friction_llim" : 0.3,
+    "friction_ulim" : 1.0,  
+
+    "front2rear_ratio" : 1.0 ,
+
+    "hinge" : 0,
+    "planet": 0,
+    "dolly" : 0,
+    "stable_len" : 0.35,
+    "fix_plans" : 1,
+    "track" : 0,
+    "planet_tread" : 0,
+
+    "winch" : 0,
+    "winch_force": 100,
+
+    "seed": 0,
+
+    "num_wheels": 8
+}
